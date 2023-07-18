3615194944ca41f17cafa62ba45edee9f502fb7d
Espen Moe-Nilssen <espen@medialog.no>
2023-07-18 13:59:27 +0200

    Update tareas_index.py

--- src/acentoweb/corpuslse/indexers/tareas_index.py
+++ src/acentoweb/corpuslse/indexers/tareas_index.py
@@ -14,4 +14,37 @@ def dummy(obj):
 @indexer(IDocument)  # ADJUST THIS!
 def tareas_index(obj):
     """Calculate and return the value for the indexer"""
-    return obj.tareas_index
+    indeks = set()
+    indeksfields = [
+        'pres_codigo',
+        'edu_codigo',
+        'infan_codigo',
+        'tya_codigo',
+        'rana_codigo',
+        'nico_codigo',
+        'simon_codigo',
+        'nev_codigo',
+        'chyc_codigo',
+        'syp_codigo',
+        'tyj_codigo',
+        'teo_codigo',
+        'drama_codigo',
+        'pera_codigo',
+        'banco_codigo',
+        'asoc_codigo',
+        'diacr_codigo'
+    ]
+
+    #def __init__(self, context, catalog):
+    #...         self.context = context
+    #...         self.catalog = catalog
+    #...
+    #...     def __call__(self):
+    #...         return len(self.context.text)
+
+    for indexfield in indeksfields:
+        if getattr(obj, indexfield):
+            indeks.add(getattr(obj, indexfield))
+
+    obj.tareas = indeks
+    return indeks
